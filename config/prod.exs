246bd8caa8edee72fdaace345afb2932c7655c1a
Tom Liversidge <tom@liversidge.me>
2017-02-17 16:26:21 +0200

    adding heroku deployment

--- config/prod.exs
+++ config/prod.exs
@@ -13,12 +13,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :circle_heroku, CircleHeroku.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
-
+  url: [scheme: "https", host: "quiet-thicket-30888.herokuapp.com", port: 443],
+force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Configure your database
+config :circle_heroku, CircleHeroku.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
@@ -58,4 +65,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
